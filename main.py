3377dd4cd5bffd54adcc377749562f8f6f7f14ba
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2021-12-31 06:01:40 +0900

    Remove extras

--- main.py
+++ main.py
@@ -20,46 +20,17 @@ df = pd.read_csv(io.BytesIO(csvResponse.content), sep=",", index_col=0, encoding
 
 df=pd.read_csv("outcome.csv",index_col=0,encoding="utf-8")
 
-print(df.describe())
-print(df.columns)
-print(df.shape)
-print(df.index)
-
-'''
-fig=plt.figure()
-axes=fig.subplots(1,2)
-df.stars.plot.box(ax=axes[0])
-df[df["stars"]>10].stars.plot.box(ax=axes[1])
-print(plt.show())
-'''
-
 # 必要なカラムを選択
 df=df[['bpm','duration','difficulty','sageScore','njs',
        'offset','notes','bombs','obstacles','nps','length','events','chroma','warns',
        'resets','stars']]
 
-print(df.columns)
-
-print(df.difficulty.unique())
-print(df.difficulty.value_counts())
-
-'''
-print(df.createdAt.head())
-df["createdAt"]=pd.to_datetime(df["createdAt"])
-print(df.createdAt)
-'''
-
 for i in [ 'difficulty', 'chroma']:
     # 同じ値が同じ数字になり、違う値が違う数値になる
     df[i]=df[i].factorize()[0]
-    print(df[i].head())
 
-# 欠損値確認
-print(df.isnull().any())
 # 欠損値を含む行を削除
 df=df.dropna(how='any')
-# 欠損値再確認
-print(df.isnull().any())
 
 # bpm1000以上を除外
 df=df[df['bpm']<1000]
@@ -88,16 +59,11 @@ columns=df.drop(labels=['stars'],axis=1).columns
 
 x_train,x_test,t_train,t_test=train_test_split(x,t,test_size=0.3,random_state=0)
 
-print(x_train.shape)
-print(x_test.shape)
-print(t_train.shape)
-print(t_test.shape)
-
 # 学習
 model=LinearRegression()
 model.fit(x_train,t_train)
 
-print(model.coef_)
+print(f"coef_: {model.coef_}")
 plt.figure(tight_layout=True)
 plt.rcParams['figure.subplot.bottom'] = 0.15
 plt.bar(x=columns,height=model.coef_)
@@ -119,7 +85,6 @@ with open('modelScore.json',mode='w') as f:
 
 
 # 多重共線性(入力変数同士の相関)の確認
-print(df.corr())
 plt.figure(tight_layout=True,figsize=[9.6,8])
 sns.heatmap(df.corr(),annot=True)
 plt.savefig("corr")

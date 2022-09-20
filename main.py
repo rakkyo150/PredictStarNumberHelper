dc271c10175c71dfb4fcfd7da8302bdadc6c14b0
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2022-09-20 12:19:41 +0900

    Cleanup

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.neural_network import MLPRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error
-import numpy as np
-import seaborn as sns
-import pickle
-import requests
 import io
 import json
 import os
+import pickle
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+import seaborn as sns
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.neural_network import MLPRegressor
 
 githubEndpoint = "https://api.github.com/repos/rakkyo150/RankedMapData/releases/latest"
-headers={'Authorization': f'token {os.environ["GITHUB_TOKEN"]}'}
-githubResponse = requests.get(url=githubEndpoint,headers=headers)
+headers = {'Authorization': f'token {os.environ["GITHUB_TOKEN"]}'}
+githubResponse = requests.get(url=githubEndpoint, headers=headers)
 releaseJson = githubResponse.json()
 secondHeaders = {'Accept': 'application/octet-stream'}
 csvResponse = requests.get(url=releaseJson["assets"][0]["browser_download_url"],
@@ -26,9 +25,9 @@ df = pd.read_csv(io.BytesIO(csvResponse.content), sep=",", index_col=0, encoding
 # df=pd.read_csv("outcome.csv",index_col=0,encoding="utf-8")
 
 # 必要なカラムを選択
-df=df[['bpm','duration','difficulty','sageScore','njs',
-       'offset','notes','bombs','obstacles','nps','events','chroma','errors','warns',
-       'resets','stars']]
+df = df[['bpm', 'duration', 'difficulty', 'sageScore', 'njs',
+         'offset', 'notes', 'bombs', 'obstacles', 'nps', 'events', 'chroma', 'errors', 'warns',
+         'resets', 'stars']]
 
 '''
 # 箱ひげ図で外れ値の確認
@@ -41,22 +40,22 @@ plt.close()
 '''
 
 # bpm1000以上を除外
-df=df[df['bpm']<1000]
+df = df[df['bpm'] < 1000]
 
 # 欠損値を含む行を削除
-df=df.dropna(how='any')
+df = df.dropna(how='any')
 
 # 同じ値が同じ数字になり、違う値が違う数値になる
 print(df['chroma'].value_counts())
-df['chroma']=df['chroma'].factorize(sort=True)[0]
+df['chroma'] = df['chroma'].factorize(sort=True)[0]
 print(df['chroma'].value_counts())
 
 print(df['difficulty'].value_counts())
-df.loc[df['difficulty']=="Easy",'difficulty']=0
-df.loc[df['difficulty']=="Normal",'difficulty']=1
-df.loc[df['difficulty']=="Hard",'difficulty']=2
-df.loc[df['difficulty']=="Expert",'difficulty']=3
-df.loc[df['difficulty']=="ExpertPlus",'difficulty']=4
+df.loc[df['difficulty'] == "Easy", 'difficulty'] = 0
+df.loc[df['difficulty'] == "Normal", 'difficulty'] = 1
+df.loc[df['difficulty'] == "Hard", 'difficulty'] = 2
+df.loc[df['difficulty'] == "Expert", 'difficulty'] = 3
+df.loc[df['difficulty'] == "ExpertPlus", 'difficulty'] = 4
 print(df['difficulty'].value_counts())
 
 # 型がobjectになっているので
@@ -67,26 +66,27 @@ print(describe)
 
 print(df.dtypes)
 
-with open('./describe.json',mode='w') as f:
-    describe.to_json(f,indent=4)
+with open('./describe.json', mode='w') as f:
+    describe.to_json(f, indent=4)
 
 # データセット分割
-t=df['stars'].values
-x=df.drop(labels=['stars'],axis=1).values
-columns=df.drop(labels=['stars'],axis=1).columns
+t = df['stars'].values
+x = df.drop(labels=['stars'], axis=1).values
+columns = df.drop(labels=['stars'], axis=1).columns
 
-x_train_val,x_test,t_train_val,t_test=train_test_split(x,t,test_size=0.2,random_state=0)
+x_train_val, x_test, t_train_val, t_test = train_test_split(x, t, test_size=0.2, random_state=0)
 
 print(x_test.shape)
 
 # 標準化
 from sklearn.preprocessing import StandardScaler
-standardScaler=StandardScaler()
-normalized_x_train_val=standardScaler.fit_transform(x_train_val)
+
+standardScaler = StandardScaler()
+normalized_x_train_val = standardScaler.fit_transform(x_train_val)
 
 print(standardScaler.mean_)
 print(standardScaler.var_)
-normalized_x_test=(x_test-standardScaler.mean_)/np.sqrt(standardScaler.var_)
+normalized_x_test = (x_test - standardScaler.mean_) / np.sqrt(standardScaler.var_)
 
 with open('./standardScaler.pickle', mode='wb') as f:
     pickle.dump(standardScaler, f)
@@ -101,25 +101,25 @@ with open('./standardScaler.pickle', mode='wb') as f:
 # "testScore": 0.9074964858127781
 
 
-estimator=MLPRegressor(random_state=0,max_iter=1000,solver="adam",activation="relu",verbose=True)
-param_grid=[{
-    'solver':['sgd'],
-    'max_iter':[1000],
-    'hidden_layer_sizes':[1500],
+estimator = MLPRegressor(random_state=0, max_iter=1000, solver="adam", activation="relu",
+                         verbose=True)
+param_grid = [{
+    'solver': ['sgd'],
+    'max_iter': [1000],
+    'hidden_layer_sizes': [1500],
 }]
-cv=5
-tuned_model=GridSearchCV(estimator=estimator,
-                         param_grid=param_grid,
-                         cv=cv,
-                         return_train_score=False)
+cv = 5
+tuned_model = GridSearchCV(estimator=estimator,
+                           param_grid=param_grid,
+                           cv=cv,
+                           return_train_score=False)
 
-tuned_model.fit(normalized_x_train_val,t_train_val)
+tuned_model.fit(normalized_x_train_val, t_train_val)
 
 print(tuned_model.best_params_)
-model=tuned_model.best_estimator_
+model = tuned_model.best_estimator_
 
-
-train_pred=model.predict(normalized_x_train_val)
+train_pred = model.predict(normalized_x_train_val)
 pred = model.predict(normalized_x_test)
 
 # old bad way
@@ -128,10 +128,10 @@ pred = model.predict(normalized_x_test)
 # train score: 0.9683632363282518
 # test score: 0.9332618120022189
 
-print(f'train RMSE: {np.sqrt(mean_squared_error(t_train_val,train_pred))}')
+print(f'train RMSE: {np.sqrt(mean_squared_error(t_train_val, train_pred))}')
 print(f'test RMSE: {np.sqrt(mean_squared_error(t_test, pred))}')
-print(f'train score: {model.score(normalized_x_train_val,t_train_val)}')
-print(f'test score: {model.score(normalized_x_test,t_test)}')
+print(f'train score: {model.score(normalized_x_train_val, t_train_val)}')
+print(f'test score: {model.score(normalized_x_test, t_test)}')
 
 # new appropriate way
 # train RMSE: 0.6312606845960649
@@ -142,24 +142,23 @@ print(f'test score: {model.score(normalized_x_test,t_test)}')
 for i in range(t_test.shape[0]):
     c = t_test[i]
     p = pred[i]
-    print('[{0}] correct:{1:.3f}, predict:{2:.3f} ({3:.3f})'.format(i, c, p, c-p))
+    print('[{0}] correct:{1:.3f}, predict:{2:.3f} ({3:.3f})'.format(i, c, p, c - p))
 
-str={
-    'train RMSE': np.sqrt(mean_squared_error(t_train_val,train_pred)),
+str = {
+    'train RMSE': np.sqrt(mean_squared_error(t_train_val, train_pred)),
     'test RMSE': np.sqrt(mean_squared_error(t_test, pred)),
-    "trainScore":model.score(x_train_val,t_train_val),
-    "testScore":model.score(x_test,t_test)
+    "trainScore": model.score(x_train_val, t_train_val),
+    "testScore": model.score(x_test, t_test)
 }
-with open('./modelEvaluation.json',mode='w') as f:
-    json.dump(str,f,indent=4)
-
+with open('./modelEvaluation.json', mode='w') as f:
+    json.dump(str, f, indent=4)
 
 # 多重共線性(入力変数同士の相関)の確認
-plt.figure(tight_layout=True,figsize=[9.6,8])
-sns.heatmap(df.corr(),annot=True)
+plt.figure(tight_layout=True, figsize=[9.6, 8])
+sns.heatmap(df.corr(), annot=True)
 plt.savefig("./correlation.png")
 plt.close()
 
 # 学習済みモデルを保存
 with open('./model.pickle', mode='wb') as f:
-    pickle.dump(model,f)
+    pickle.dump(model, f)
